8ffa2cac554e55752e892fbebb5ca9d5bcf65295
Ronn Mico Lee <69786678+LeeDevDevDev@users.noreply.github.com>
2022-09-01 15:27:42 +0800

    removed environments in gitignore 🙈

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  supabaseUrl: 'https://ioxhyweqedmptfntzfja.supabase.co',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlveGh5d2VxZWRtcHRmbnR6ZmphIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjIwMTMxNDEsImV4cCI6MTk3NzU4OTE0MX0.qaMdQEG5YY-CmkkVeYJjhmKXnE89L6QcqbhWZl4WXzw.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlveGh5d2VxZWRtcHRmbnR6ZmphIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjIwMTMxNDEsImV4cCI6MTk3NzU4OTE0MX0.qaMdQEG5YY-CmkkVeYJjhmKXnE89L6QcqbhWZl4WXzw',
+};
